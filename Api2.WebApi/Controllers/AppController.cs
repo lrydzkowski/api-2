10ba06b27577a7a0aa18338d47131d47ae74e762
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2022-06-16 11:57:03 +0200

    init: Api2

---
+++ Api2.WebApi/Controllers/AppController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api2.WebApi.Controllers;
+
+[ApiController]
+public class AppController : ControllerBase
+{
+    [HttpGet]
+    [Route("app")]
+    public IActionResult GetAppInfo()
+    {
+        return Ok(new { Version = "1.0.0", Name = "Api2" });
+    }
+}
